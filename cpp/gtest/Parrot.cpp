b151032b96a720c613c670ed5586242c8490bf98
pgrlol <p.graser@gmx.at>
2021-12-22 11:50:20 +0100

    added NorwegianBlueParrot constructor implementation

--- cpp/gtest/Parrot.cpp
+++ cpp/gtest/Parrot.cpp
@@ -53,3 +53,6 @@ EuropeanParrot::EuropeanParrot(int numberOfCoconuts, double voltage, bool isNail
 
 AfricanParrot::AfricanParrot(int numberOfCoconuts, double voltage, bool isNailed)
     : Parrot(ParrotType::AFRICAN, numberOfCoconuts, voltage, isNailed) {}
+
+NorwegianBlueParrot::NorwegianBlueParrot(int numberOfCoconuts, double voltage, bool isNailed)
+    : Parrot(ParrotType::NORWEGIAN_BLUE, numberOfCoconuts, voltage, isNailed) {}
