55139372036188bea55bb89e63cc2356c601faaf
Lukas M Schaefer <lukas.m.schaefer@untis.at>
2021-12-22 10:10:23 +0100

    emptyline

--- cpp/gtest/Parrot.cpp
+++ cpp/gtest/Parrot.cpp
@@ -8,6 +8,7 @@ using namespace std;
 Parrot::Parrot(ParrotType parrotType, int numberOfCoconuts, double voltage, bool isNailed)
 : parrotType(parrotType), numberOfCoconuts(numberOfCoconuts), voltage(voltage), isNailed(isNailed) {}
 
+
 double Parrot::getSpeed() {
     switch (parrotType) {
         case EUROPEAN:
