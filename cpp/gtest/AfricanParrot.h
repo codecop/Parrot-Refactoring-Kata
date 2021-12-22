f8543f1944a226cf931deae2e116177b1ca81617
Lukas M Schaefer <lukas.m.schaefer@untis.at>
2021-12-22 11:04:48 +0100

    Add empty African

---
+++ cpp/gtest/AfricanParrot.h
@@ -0,0 +1,12 @@
+#ifndef AFRICANPARROT_PARROT_H
+#define AFRICANPARROT_PARROT_H
+
+#include "Parrot.h"
+
+class AfricanParrot : public Parrot {
+public:
+    AfricanParrot() {};
+};
+
+
+#endif //PARROT_PARROT_H
