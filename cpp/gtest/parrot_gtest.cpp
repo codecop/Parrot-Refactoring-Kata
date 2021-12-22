e45bed4f18bc24329d058ec6d0d38e32d9edbf3f
Markus Mayr <markus.mayr@untis.at>
2021-12-22 12:07:49 +0100

    Pull out epsilon in unit test floating point comparisons

--- cpp/gtest/parrot_gtest.cpp
+++ cpp/gtest/parrot_gtest.cpp
@@ -4,37 +4,39 @@
 
 #include "Parrot.h"
 
+static constexpr double epsilon = 0.0;
+
 TEST(ParrotTest, SpeedOfEuropeanParrot) {
     auto parrot = std::unique_ptr<EuropeanParrot>(new EuropeanParrot{});
-    EXPECT_NEAR(12.0, parrot->getSpeed(), 0.0);
+    EXPECT_NEAR(12.0, parrot->getSpeed(), epsilon);
 }
 
 TEST(ParrotTest, SpeedOfAfricanParrot_With_One_Coconut) {
     auto parrot = std::unique_ptr<AfricanParrot>(new AfricanParrot{ 1 });
-    EXPECT_NEAR(3.0, parrot->getSpeed(), 0.0);
+    EXPECT_NEAR(3.0, parrot->getSpeed(), epsilon);
 }
 
 TEST(ParrotTest, SpeedOfAfricanParrot_With_Two_Coconuts) {
     auto parrot = std::unique_ptr<AfricanParrot>(new AfricanParrot{ 2 });
-    EXPECT_NEAR(0.0, parrot->getSpeed(), 0.0);
+    EXPECT_NEAR(0.0, parrot->getSpeed(), epsilon);
 }
 
 TEST(ParrotTest, SpeedOfAfricanParrot_With_No_Coconuts) {
     auto parrot = std::unique_ptr<AfricanParrot>(new AfricanParrot{ 0 });
-    EXPECT_NEAR(12.0, parrot->getSpeed(), 0.0);
+    EXPECT_NEAR(12.0, parrot->getSpeed(), epsilon);
 }
 
 TEST(ParrotTest, SpeedNorwegianBlueParrot_nailed) {
     auto parrot = std::unique_ptr<NorwegianBlueParrot>(new NorwegianBlueParrot{ 1.5, true });
-    EXPECT_NEAR(0.0, parrot->getSpeed(), 0.0);
+    EXPECT_NEAR(0.0, parrot->getSpeed(), epsilon);
 }
 
 TEST(ParrotTest, SpeedNorwegianBlueParrot_not_nailed) {
     auto parrot = std::unique_ptr<NorwegianBlueParrot>(new NorwegianBlueParrot{ 1.5, false });
-    EXPECT_NEAR(18.0, parrot->getSpeed(), 0.0);
+    EXPECT_NEAR(18.0, parrot->getSpeed(), epsilon);
 }
 
 TEST(ParrotTest, SpeedNorwegianBlueParrot_not_nailed_high_voltage) {
     auto parrot = std::unique_ptr<NorwegianBlueParrot>(new NorwegianBlueParrot{ 4, false });
-    EXPECT_NEAR(24.0, parrot->getSpeed(), 0.0);
+    EXPECT_NEAR(24.0, parrot->getSpeed(), epsilon);
 }
