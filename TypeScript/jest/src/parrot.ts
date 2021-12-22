57858f8e1e90c526046066bc9a8ba3968dd9a9bf
Florian Kapfenberger <florian@kapfenberger.me>
2021-12-22 12:11:28 +0100

    add default case to please TS

--- TypeScript/jest/src/parrot.ts
+++ TypeScript/jest/src/parrot.ts
@@ -34,6 +34,8 @@ export class Parrot {
         return new AfricanParrot(this.numberOfCoconuts).getSpeed();
       case ParrotTypes.NORWEGIAN_BLUE:
         return new NorwegianBlueParrot(this.voltage, this.isNailed).getSpeed();
+    default:
+        return this.MIN_SPEED;
     }
   }
 
