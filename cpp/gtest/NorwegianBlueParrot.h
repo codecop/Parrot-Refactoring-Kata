a9f1a0deb109afea992f662a4576edd31630f26d
Lukas M Schaefer <lukas.m.schaefer@untis.at>
2021-12-22 11:29:21 +0100

    empty norwegianblue class

---
+++ cpp/gtest/NorwegianBlueParrot.h
@@ -0,0 +1,13 @@
+#ifndef NORWEGIANBLUEPARROT_PARROT_H
+#define NORWEGIANBLUEPARROT_PARROT_H
+
+
+#include "ParrotBase.h"
+
+class NorwegianBlueParrot : public ParrotBase {
+public:
+    NorwegianBlueParrot() {};
+};
+
+
+#endif //NORWEGIANBLUEPARROT_PARROT_H
