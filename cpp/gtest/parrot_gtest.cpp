204a31bf24ec3da2ca39fa75e99894ea493f730c
Markus Mayr <markus.mayr@untis.at>
2021-12-22 11:54:48 +0100

    Replace new with make_unique in unit tests

--- cpp/gtest/parrot_gtest.cpp
+++ cpp/gtest/parrot_gtest.cpp
@@ -1,48 +1,43 @@
 #include <gtest/gtest.h>
 
+#include <memory>
+
 using namespace std;
 
 #include "Parrot.h"
 
 
 TEST(ParrotTest, SpeedOfEuropeanParrot) {
-    auto* parrot = new EuropeanParrot();
+    auto parrot = std::make_unique<EuropeanParrot>();
     EXPECT_NEAR(12.0, parrot->getSpeed(), 0.0);
-    delete parrot;
 }
 
 TEST(ParrotTest, SpeedOfAfricanParrot_With_One_Coconut) {
-    auto* parrot = new AfricanParrot(1);
+    auto parrot = std::make_unique<AfricanParrot>(1);
     EXPECT_NEAR(3.0, parrot->getSpeed(), 0.0);
-    delete parrot;
 }
 
 TEST(ParrotTest, SpeedOfAfricanParrot_With_Two_Coconuts) {
-    auto* parrot = new AfricanParrot(2);
+    auto parrot = std::make_unique<AfricanParrot>(2);
     EXPECT_NEAR(0.0, parrot->getSpeed(), 0.0);
-    delete parrot;
 }
 
 TEST(ParrotTest, SpeedOfAfricanParrot_With_No_Coconuts) {
-    auto* parrot = new AfricanParrot(0);
+    auto parrot = std::make_unique<AfricanParrot>(0);
     EXPECT_NEAR(12.0, parrot->getSpeed(), 0.0);
-    delete parrot;
 }
 
 TEST(ParrotTest, SpeedNorwegianBlueParrot_nailed) {
-    auto *parrot = new NorwegianBlueParrot(1.5, true);
+    auto parrot = std::make_unique<NorwegianBlueParrot>(1.5, true);
     EXPECT_NEAR(0.0, parrot->getSpeed(), 0.0);
-    delete parrot;
 }
 
 TEST(ParrotTest, SpeedNorwegianBlueParrot_not_nailed) {
-    auto *parrot = new NorwegianBlueParrot(1.5, false);
+    auto parrot = std::make_unique<NorwegianBlueParrot>(1.5, false);
     EXPECT_NEAR(18.0, parrot->getSpeed(), 0.0);
-    delete parrot;
 }
 
 TEST(ParrotTest, SpeedNorwegianBlueParrot_not_nailed_high_voltage) {
-    auto *parrot = new NorwegianBlueParrot(4, false);
+    auto parrot = std::make_unique<NorwegianBlueParrot>(4, false);
     EXPECT_NEAR(24.0, parrot->getSpeed(), 0.0);
-    delete parrot;
 }
