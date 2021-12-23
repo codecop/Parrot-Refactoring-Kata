efa91e7b73b8dac7bb1ef303f056bd5693cb81e5
Andreas Denkmayr <andreas.denkmayr@gmail.com>
2021-12-23 10:51:23 +0100

    refactor: copied STANDING_IN_PLACE to Speedable

--- Java/src/main/java/parrot/Speedable.java
+++ Java/src/main/java/parrot/Speedable.java
@@ -5,6 +5,7 @@ public interface Speedable {
     double LOAD_FACTOR = 9.0;
     double BASE_SPEED = 12.0;
     double MINIMUM_SPEED = 24.0;
+    int STANDING_IN_PLACE = 0;
 
     double getSpeed();
 }
