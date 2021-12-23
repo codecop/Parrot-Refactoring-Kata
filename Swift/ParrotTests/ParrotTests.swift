1bb51850560f959f22eb890af793ad699dd85670
Alexandros Lykesas <alexandros.lykesas@untis.at>
2021-12-23 11:40:53 +0200

    Replace Parrot to NorwegianBlueParrot

--- Swift/ParrotTests/ParrotTests.swift
+++ Swift/ParrotTests/ParrotTests.swift
@@ -22,17 +22,17 @@ class ParrotTests: XCTestCase {
     }
     
     func testSpeedOfNorwegianBlueParrot_nailed() {
-        let parrot = Parrot(.norwegianBlue, numberOfCoconuts: 0, voltage: 0.0, isNailed: true);
+        let parrot = NorwegianBlueParrot(.norwegianBlue, numberOfCoconuts: 0, voltage: 0.0, isNailed: true);
         XCTAssertEqual(parrot.speed, 0.0)
     }
     
     func testSpeedOfNorwegianBlueParrot_not_nailed() {
-        let parrot = Parrot(.norwegianBlue, numberOfCoconuts: 0, voltage: 1.5, isNailed: false);
+        let parrot = NorwegianBlueParrot(.norwegianBlue, numberOfCoconuts: 0, voltage: 1.5, isNailed: false);
         XCTAssertEqual(parrot.speed, 18.0)
     }
 
     func testSpeedOfNorwegianBlueParrot_not_nailed_high_voltage() {
-        let parrot = Parrot(.norwegianBlue, numberOfCoconuts: 0, voltage: 4.0, isNailed: false);
+        let parrot = NorwegianBlueParrot(.norwegianBlue, numberOfCoconuts: 0, voltage: 4.0, isNailed: false);
         XCTAssertEqual(parrot.speed, 24.0)
     }
 }
