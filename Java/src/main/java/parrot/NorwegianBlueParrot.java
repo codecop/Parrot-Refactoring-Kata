7b45f857d0cd4b51bd223c41d5b7d91f72fbdff4
Andreas Denkmayr <andreas.denkmayr@gmail.com>
2021-12-23 11:30:57 +0100

    refactor: added voltage to NorwegianBlueParrot

--- Java/src/main/java/parrot/NorwegianBlueParrot.java
+++ Java/src/main/java/parrot/NorwegianBlueParrot.java
@@ -4,6 +4,9 @@ import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 public class NorwegianBlueParrot implements Speedable{
+
+    private final double voltage;
+
     @Override
     public double getSpeed() {
         return 0;
