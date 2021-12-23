f7a35e2e615c830d30297602373928d2bda267ea
Andreas Denkmayr <andreas.denkmayr@gmail.com>
2021-12-23 11:48:16 +0100

    refactor: override getSpeed()

--- Java/src/main/java/parrot/AfricanParrot.java
+++ Java/src/main/java/parrot/AfricanParrot.java
@@ -4,4 +4,9 @@ public class AfricanParrot extends Parrot implements Speedable{
     public AfricanParrot(ParrotType parrotType, int numberOfCoconuts, double voltage, boolean isNailed) {
         super(parrotType, numberOfCoconuts, voltage, isNailed);
     }
+
+    @Override
+    public double getSpeed() {
+        return super.getSpeed();
+    }
 }
