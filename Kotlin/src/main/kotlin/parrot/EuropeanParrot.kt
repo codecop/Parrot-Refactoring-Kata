6418eab6fd22f39cf6dfb11da6d08fe78a6cc6c2
Daniel Rajs <daniel.rajs@untis.at>
2021-12-23 10:31:55 +0100

    make european parrot subclass of parrot

--- Kotlin/src/main/kotlin/parrot/EuropeanParrot.kt
+++ Kotlin/src/main/kotlin/parrot/EuropeanParrot.kt
@@ -2,5 +2,11 @@ package parrot
 
 class EuropeanParrot(
     numberOfCoconuts: Int,
-    voltage: Double
+    voltage: Double,
+    isNailed: Boolean
+) : Parrot(
+    type = ParrotTypeEnum.EUROPEAN,
+    numberOfCoconuts = numberOfCoconuts,
+    voltage = voltage,
+    isNailed = isNailed
 )
