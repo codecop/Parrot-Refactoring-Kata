62154ceb02e1d61b0478aa37e3e729ec9f245c25
Andreas Denkmayr <andreas.denkmayr@gmail.com>
2021-12-23 11:58:16 +0100

    refactor: removed unused constructor

--- Java/src/main/java/parrot/NorwegianBlueParrot.java
+++ Java/src/main/java/parrot/NorwegianBlueParrot.java
@@ -7,12 +7,6 @@ public class NorwegianBlueParrot extends Parrot implements Speedable {
     private double voltage;
     private boolean isNailed;
 
-    public NorwegianBlueParrot(ParrotType parrotType, int numberOfCoconuts, double voltage, boolean isNailed) {
-        super(parrotType, numberOfCoconuts, voltage, isNailed);
-        this.voltage = voltage;
-        this.isNailed = isNailed;
-    }
-
     @Override
     public double getSpeed() {
         return (isNailed) ? STANDING_IN_PLACE : getBaseSpeed(voltage);
