8dda5b32c9af6d0ab1670641f4aaea38424731ca
Andreas Denkmayr <andreas.denkmayr@gmail.com>
2021-12-23 12:01:37 +0100

    refactor: dropped unused Exception

--- Java/src/main/java/parrot/UnknownParrotSpeciesException.java
+++
@@ -1,8 +0,0 @@
-package parrot;
-
-public class UnknownParrotSpeciesException extends RuntimeException {
-
-    public UnknownParrotSpeciesException() {
-        super("This Parrot Species is unknown!");
-    }
-}
