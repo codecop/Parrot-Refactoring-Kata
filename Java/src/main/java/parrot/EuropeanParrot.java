a2f92f20e4812271617af1ca9759e54d75c53582
Andreas Denkmayr <andreas.denkmayr@gmail.com>
2021-12-23 11:47:18 +0100

    refactor: removed unused constructor

--- Java/src/main/java/parrot/EuropeanParrot.java
+++ Java/src/main/java/parrot/EuropeanParrot.java
@@ -5,10 +5,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class EuropeanParrot extends Parrot implements Speedable{
 
-    public EuropeanParrot(ParrotType parrotType, int numberOfCoconuts, double voltage, boolean isNailed) {
-        super(parrotType, numberOfCoconuts, voltage, isNailed);
-    }
-
     @Override
     public double getSpeed() {
         return BASE_SPEED;
