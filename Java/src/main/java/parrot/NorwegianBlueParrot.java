91095dc38efc44343fbb5773d2cb33371902f75e
Stefan Wagner <stw@untis.at>
2021-12-23 11:53:37 +0100

    moved basespeed

--- Java/src/main/java/parrot/NorwegianBlueParrot.java
+++ Java/src/main/java/parrot/NorwegianBlueParrot.java
@@ -16,4 +16,9 @@ public class NorwegianBlueParrot extends Parrot {
     public double getSpeed() {
         return (isNailed) ? 0 : getBaseSpeed(voltage);
     }
+
+    @Override
+    protected double getBaseSpeed(double voltage) {
+        return Math.min(24.0, voltage * getBaseSpeed());
+    }
 }
