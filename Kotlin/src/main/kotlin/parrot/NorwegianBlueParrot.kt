b0508f2a1f50451fc64bb14029f9450614a7aca6
Daniel Rajs <daniel.rajs@untis.at>
2021-12-23 10:57:20 +0100

    remove magic value : nailed speed

--- Kotlin/src/main/kotlin/parrot/NorwegianBlueParrot.kt
+++ Kotlin/src/main/kotlin/parrot/NorwegianBlueParrot.kt
@@ -3,11 +3,14 @@ package parrot
 class NorwegianBlueParrot(
     voltage: Double,
     private val isNailed: Boolean
-): Parrot(
+) : Parrot(
     voltage = voltage
 ) {
 
+    private val nailedSpeed: Double
+        get() = 0.0
+
     override val speed: Double
-        get() = if (isNailed) 0.0 else getBaseSpeed(voltage)
+        get() = if (isNailed) nailedSpeed else getBaseSpeed(voltage)
 
 }
