ed93e70ac7c57e65d8fb4a200ed09e8d5d5dc7fc
Andreas Denkmayr <andreas.denkmayr@gmail.com>
2021-12-23 11:49:45 +0100

    refactor: added numberOfCoconuts

--- Java/src/main/java/parrot/AfricanParrot.java
+++ Java/src/main/java/parrot/AfricanParrot.java
@@ -1,8 +1,12 @@
 package parrot;
 
 public class AfricanParrot extends Parrot implements Speedable{
+
+    private final int numberOfCoconuts;
+
     public AfricanParrot(ParrotType parrotType, int numberOfCoconuts, double voltage, boolean isNailed) {
         super(parrotType, numberOfCoconuts, voltage, isNailed);
+        this.numberOfCoconuts = numberOfCoconuts;
     }
 
     @Override
