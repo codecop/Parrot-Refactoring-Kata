4b74cd8cb6918be07b628192d0f96348be55888e
Andreas Denkmayr <andreas.denkmayr@gmail.com>
2021-12-23 11:05:29 +0100

    refactor: added numberOfCoconuts to AfricanParrot

--- Java/src/main/java/parrot/AfricanParrot.java
+++ Java/src/main/java/parrot/AfricanParrot.java
@@ -4,6 +4,9 @@ import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 public class AfricanParrot implements Speedable {
+
+    private final int numberOfCoconuts;
+
     @Override
     public double getSpeed() {
         return 0;
