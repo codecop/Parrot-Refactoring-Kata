6ccd6abb92c9ba919d69edb29674d9a95bcf1614
Martin Blum <martin.blum@untis.at>
2021-12-23 10:28:23 +0100

    constant LOAD_FACTOR

--- Java/src/main/java/parrot/speedcalculators/AfricanParrotSpeedCalculator.java
+++ Java/src/main/java/parrot/speedcalculators/AfricanParrotSpeedCalculator.java
@@ -2,6 +2,9 @@ package parrot.speedcalculators;
 
 // Math.max(0, getBaseSpeed() - getLoadFactor() * numberOfCoconuts);
 public class AfricanParrotSpeedCalculator implements ParrotSpeedCalulator {
+
+    public static double LOAD_FACTOR = 9.0;
+
     @Override
     public double getSpeed() {
         return 0;
