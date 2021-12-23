72898113fceb4b08cf0e890590d8f276a7beb693
Günther Bernsteiner <gb@untis.at>
2021-12-23 12:15:35 +0100

    Calculator african tests

--- Java/src/test/java/parrot/ParrotTest.java
+++ Java/src/test/java/parrot/ParrotTest.java
@@ -6,5 +6,22 @@ import static org.junit.Assert.assertEquals;
 
 public class ParrotTest {
 
+    @Test
+    public void getSpeedOfAfricanParrot_With_One_Coconut() {
+        Calculator parrot = new Calculator(new AfricanParrot(1));
+        assertEquals(3.0, parrot.getSpeed(), 0.0);
+    }
+
+    @Test
+    public void getSpeedOfAfricanParrot_With_Two_Coconuts() {
+        Calculator parrot = new Calculator(new AfricanParrot(2));
+        assertEquals(0.0, parrot.getSpeed(), 0.0);
+    }
+
+    @Test
+    public void getSpeedOfAfricanParrot_With_No_Coconuts() {
+        Calculator parrot = new Calculator(new AfricanParrot(0));
+        assertEquals(12.0, parrot.getSpeed(), 0.0);
+    }
 
 }
