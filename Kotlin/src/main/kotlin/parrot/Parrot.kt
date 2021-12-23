6fc73290918a68c04f358fedf769123582a5074f
Daniel Rajs <daniel.rajs@untis.at>
2021-12-23 11:28:05 +0100

    move getBaseSpeed to norwegian

--- Kotlin/src/main/kotlin/parrot/Parrot.kt
+++ Kotlin/src/main/kotlin/parrot/Parrot.kt
@@ -1,7 +1,5 @@
 package parrot
 
-import kotlin.math.min
-
 abstract class Parrot(
     protected val voltage: Double
 ) {
@@ -11,10 +9,6 @@ abstract class Parrot(
     protected val baseSpeed: Double
         get() = 12.0
 
-    private val minBaseSpeed: Double
-        get() = 24.0
-
-    protected fun getBaseSpeed(voltage: Double): Double = min(minBaseSpeed, voltage * baseSpeed)
 }
 
 
