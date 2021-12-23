bb580cd3998d2600872bbb7a94999b7e22363ce6
Andreas Denkmayr <andreas.denkmayr@gmail.com>
2021-12-23 11:58:57 +0100

    refactor: copied getBaseSpeed()

--- Java/src/main/java/parrot/NorwegianBlueParrot.java
+++ Java/src/main/java/parrot/NorwegianBlueParrot.java
@@ -11,4 +11,9 @@ public class NorwegianBlueParrot extends Parrot implements Speedable {
     public double getSpeed() {
         return (isNailed) ? STANDING_IN_PLACE : getBaseSpeed(voltage);
     }
+
+    @Override
+    protected double getBaseSpeed(double voltage) {
+        return Math.min(MINIMUM_SPEED, voltage * BASE_SPEED);
+    }
 }
