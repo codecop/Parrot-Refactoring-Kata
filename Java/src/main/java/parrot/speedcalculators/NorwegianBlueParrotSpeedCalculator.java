514c41e8c7a8c060e12d4647a00ca43020b1748b
Martin Blum <martin.blum@untis.at>
2021-12-23 10:39:16 +0100

    more documentation

--- Java/src/main/java/parrot/speedcalculators/NorwegianBlueParrotSpeedCalculator.java
+++ Java/src/main/java/parrot/speedcalculators/NorwegianBlueParrotSpeedCalculator.java
@@ -1,6 +1,9 @@
 package parrot.speedcalculators;
 
 //return (isNailed) ? 0 : getBaseSpeed(voltage);
+// getBaseSpeed(double voltage) {
+//        return Math.min(24.0, voltage * getBaseSpeed());
+//    }
 public class NorwegianBlueParrotSpeedCalculator implements ParrotSpeedCalulator {
 
     private boolean isNailed;
