acb6b05b287bdefba9f56767adfc6e36482d5adf
Martin Blum <martin.blum@untis.at>
2021-12-23 10:34:02 +0100

    Remove documentation

--- Java/src/main/java/parrot/speedcalculators/AfricanParrotSpeedCalculator.java
+++ Java/src/main/java/parrot/speedcalculators/AfricanParrotSpeedCalculator.java
@@ -1,6 +1,5 @@
 package parrot.speedcalculators;
 
-// Math.max(0, getBaseSpeed() - getLoadFactor() * numberOfCoconuts);
 public class AfricanParrotSpeedCalculator implements ParrotSpeedCalulator {
 
     public static double LOAD_FACTOR = 9.0;
