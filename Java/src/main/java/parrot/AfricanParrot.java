020b5867fd1aa7585b51d963f0c6417a4551a6a0
Andreas Denkmayr <andreas.denkmayr@gmail.com>
2021-12-23 11:39:26 +0100

    refactor: AfricanParrot implements Speedable

--- Java/src/main/java/parrot/AfricanParrot.java
+++ Java/src/main/java/parrot/AfricanParrot.java
@@ -1,6 +1,6 @@
 package parrot;
 
-public class AfricanParrot extends Parrot {
+public class AfricanParrot extends Parrot implements Speedable{
     public AfricanParrot(ParrotType parrotType, int numberOfCoconuts, double voltage, boolean isNailed) {
         super(parrotType, numberOfCoconuts, voltage, isNailed);
     }
