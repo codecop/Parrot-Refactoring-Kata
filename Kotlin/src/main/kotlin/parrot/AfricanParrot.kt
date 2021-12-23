52360c431aae0d60495f88fc41362cd45c074583
Daniel Rajs <daniel.rajs@untis.at>
2021-12-23 11:57:44 +0100

    simplify variables for european

--- Kotlin/src/main/kotlin/parrot/AfricanParrot.kt
+++ Kotlin/src/main/kotlin/parrot/AfricanParrot.kt
@@ -6,11 +6,9 @@ class AfricanParrot(
     private val numberOfCoconuts: Int
 ) : Parrot() {
 
-    private val loadFactor: Double
-        get() = 9.0
+    private val loadFactor: Double = 9.0
 
-    private val minSpeed: Double
-        get() = 0.0
+    private val minSpeed: Double = 0.0
 
     override val speed: Double
         get() = max(minSpeed, baseSpeed - loadFactor * numberOfCoconuts)
