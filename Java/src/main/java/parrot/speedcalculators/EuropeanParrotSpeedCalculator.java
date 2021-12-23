3bd62028e490ab930b60f9f7dd0288f4263d7c80
Martin Blum <martin.blum@untis.at>
2021-12-23 10:24:52 +0100

    constant

--- Java/src/main/java/parrot/speedcalculators/EuropeanParrotSpeedCalculator.java
+++ Java/src/main/java/parrot/speedcalculators/EuropeanParrotSpeedCalculator.java
@@ -1,6 +1,9 @@
 package parrot.speedcalculators;
 
 public class EuropeanParrotSpeedCalculator implements ParrotSpeedCalulator {
+
+    public static double BASE_SPEED = 12.0;
+
     @Override
     public double getSpeed() {
         return 0;
