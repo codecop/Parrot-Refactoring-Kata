e258491ad2ef6ab4181128a99e5fb0f531193ca0
Andreas Denkmayr <andreas.denkmayr@gmail.com>
2021-12-23 10:32:06 +0100

    refactor: renamed type variable

--- Java/src/main/java/parrot/Parrot.java
+++ Java/src/main/java/parrot/Parrot.java
@@ -10,13 +10,13 @@ public class Parrot {
     public static final double LOAD_FACTOR = 9.0;
     public static final double BASE_SPEED = 12.0;
     public static final double MINIMUM_SPEED = 24.0;
-    private final ParrotType type;
+    private final ParrotType parrotType;
     private final int numberOfCoconuts;
     private final double voltage;
     private final boolean isNailed;
 
     public double getSpeed() {
-        switch (type) {
+        switch (parrotType) {
             case EUROPEAN:
                 return BASE_SPEED;
             case AFRICAN:
