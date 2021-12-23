49aa986e84424c8b4c02ac49af222074a80d91c6
Stefan Wagner <stw@untis.at>
2021-12-23 11:35:12 +0100

    european  test

--- Java/src/test/java/parrot/EuropeanParrotTest.java
+++ Java/src/test/java/parrot/EuropeanParrotTest.java
@@ -1,4 +1,14 @@
 package parrot;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class EuropeanParrotTest {
+
+    @Test
+    public void getSpeedOfEuropeanParrot() {
+        Parrot parrot = new EuropeanParrot();
+        assertEquals(12.0, parrot.getSpeed(), 0.0);
+    }
 }
