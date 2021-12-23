d80d0a2337576a224f5e78ec3b5d29ea5f8d10c4
Stefan Wagner <stw@untis.at>
2021-12-23 12:04:24 +0100

    parent unused methods remove

--- Java/src/main/java/parrot/Parrot.java
+++ Java/src/main/java/parrot/Parrot.java
@@ -17,16 +17,7 @@ public abstract class Parrot {
 
     abstract public double getSpeed();
 
-     private double getBaseSpeed(double voltage) {
-        return Math.min(24.0, voltage * getBaseSpeed());
-    }
-
-    private double getLoadFactor() {
-        return 9.0;
-    }
-
     protected double getBaseSpeed() {
         return 12.0;
     }
-
 }
