3be31d64931b8300c0a0e71d9146c4db3f749343
Martin Blum <martin.blum@untis.at>
2021-12-23 10:36:48 +0100

    constructor for norwegian parrot

--- Java/src/main/java/parrot/speedcalculators/NorwegianBlueParrotSpeedCalculator.java
+++ Java/src/main/java/parrot/speedcalculators/NorwegianBlueParrotSpeedCalculator.java
@@ -2,6 +2,9 @@ package parrot.speedcalculators;
 
 //return (isNailed) ? 0 : getBaseSpeed(voltage);
 public class NorwegianBlueParrotSpeedCalculator implements ParrotSpeedCalulator {
+    public NorwegianBlueParrotSpeedCalculator() {
+    }
+
     @Override
     public double getSpeed() {
         return 0;
