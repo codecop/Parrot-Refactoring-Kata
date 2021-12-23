419d388c9666ccba2f9f437e6d77a7223732b30e
Daniel Rajs <daniel.rajs@untis.at>
2021-12-23 11:01:26 +0100

    add named parameters to tests

--- Kotlin/src/test/kotlin/parrot/ParrotTest.kt
+++ Kotlin/src/test/kotlin/parrot/ParrotTest.kt
@@ -8,43 +8,43 @@ class ParrotTest {
 
     @Test
     fun getSpeedOfEuropeanParrot() {
-        val parrot = EuropeanParrot(0.0)
+        val parrot = EuropeanParrot(voltage = 0.0)
         assertEquals(12.0, parrot.speed, 0.0)
     }
 
     @Test
     fun getSpeedOfAfricanParrot_With_One_Coconut() {
-        val parrot = AfricanParrot(1, 0.0)
+        val parrot = AfricanParrot(numberOfCoconuts = 1, voltage = 0.0)
         assertEquals(3.0, parrot.speed, 0.0)
     }
 
     @Test
     fun getSpeedOfAfricanParrot_With_Two_Coconuts() {
-        val parrot = AfricanParrot(2, 0.0)
+        val parrot = AfricanParrot(numberOfCoconuts = 2, voltage = 0.0)
         assertEquals(0.0, parrot.speed, 0.0)
     }
 
     @Test
     fun getSpeedOfAfricanParrot_With_No_Coconuts() {
-        val parrot = AfricanParrot(0, 0.0)
+        val parrot = AfricanParrot(numberOfCoconuts = 0, voltage = 0.0)
         assertEquals(12.0, parrot.speed, 0.0)
     }
 
     @Test
     fun getSpeedNorwegianBlueParrot_nailed() {
-        val parrot = NorwegianBlueParrot(1.5, true)
+        val parrot = NorwegianBlueParrot(voltage = 1.5, isNailed = true)
         assertEquals(0.0, parrot.speed, 0.0)
     }
 
     @Test
     fun getSpeedNorwegianBlueParrot_not_nailed() {
-        val parrot = NorwegianBlueParrot(1.5, false)
+        val parrot = NorwegianBlueParrot(voltage = 1.5, isNailed = false)
         assertEquals(18.0, parrot.speed, 0.0)
     }
 
     @Test
     fun getSpeedNorwegianBlueParrot_not_nailed_high_voltage() {
-        val parrot = NorwegianBlueParrot(4.0, false)
+        val parrot = NorwegianBlueParrot(voltage = 4.0, isNailed = false)
         assertEquals(24.0, parrot.speed, 0.0)
     }
 }
