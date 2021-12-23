1308557e495a2f8020b31b3a92b102e66869e0c2
Martin Blum <martin.blum@untis.at>
2021-12-23 10:36:11 +0100

    add documentation

--- Java/src/main/java/parrot/speedcalculators/NorwegianBlueParrotSpeedCalculator.java
+++ Java/src/main/java/parrot/speedcalculators/NorwegianBlueParrotSpeedCalculator.java
@@ -1,5 +1,6 @@
 package parrot.speedcalculators;
 
+//return (isNailed) ? 0 : getBaseSpeed(voltage);
 public class NorwegianBlueParrotSpeedCalculator implements ParrotSpeedCalulator {
     @Override
     public double getSpeed() {
