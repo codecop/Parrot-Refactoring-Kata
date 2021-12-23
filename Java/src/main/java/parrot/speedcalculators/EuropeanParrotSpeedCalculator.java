46d461a12149cf18c933dd26a544a6cc96a346e7
Martin Blum <martin.blum@untis.at>
2021-12-23 10:30:30 +0100

    delete the old constand

--- Java/src/main/java/parrot/speedcalculators/EuropeanParrotSpeedCalculator.java
+++ Java/src/main/java/parrot/speedcalculators/EuropeanParrotSpeedCalculator.java
@@ -2,8 +2,6 @@ package parrot.speedcalculators;
 
 public class EuropeanParrotSpeedCalculator implements ParrotSpeedCalulator {
 
-    public static double BASE_SPEED = 12.0;
-
     @Override
     public double getSpeed() {
         return BASE_SPEED;
