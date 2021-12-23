13d6736ac38a1fa3b2c1a9419c435149b9e39eec
Daniel Rajs <daniel.rajs@untis.at>
2021-12-23 10:31:02 +0100

    make norwegian parrot subclass of parrot

--- Kotlin/src/main/kotlin/parrot/NorwegianBlueParrot.kt
+++ Kotlin/src/main/kotlin/parrot/NorwegianBlueParrot.kt
@@ -4,4 +4,9 @@ class NorwegianBlueParrot(
     numberOfCoconuts: Int,
     voltage: Double,
     isNailed: Boolean
+): Parrot(
+    type = ParrotTypeEnum.NORWEGIAN_BLUE,
+    numberOfCoconuts = numberOfCoconuts,
+    voltage = voltage,
+    isNailed = isNailed
 )
