7858c4f8c10aba960ddf0bc5046001cd8d990dea
Günther Bernsteiner <gb@untis.at>
2021-12-23 12:16:22 +0100

    Calculator european tests

--- Java/src/test/java/parrot/ParrotTest.java
+++ Java/src/test/java/parrot/ParrotTest.java
@@ -5,6 +5,11 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class ParrotTest {
+    @Test
+    public void getSpeedOfEuropeanParrot() {
+        Calculator parrot = new Calculator(new EuropeanParrot());
+        assertEquals(12.0, parrot.getSpeed(), 0.0);
+    }
 
     @Test
     public void getSpeedOfAfricanParrot_With_One_Coconut() {
