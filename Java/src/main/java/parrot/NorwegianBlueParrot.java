5d48689c7807593439597061cded5ec40fe43298
Stefan Wagner <stw@untis.at>
2021-12-23 12:00:48 +0100

    norwegian constructor

--- Java/src/main/java/parrot/NorwegianBlueParrot.java
+++ Java/src/main/java/parrot/NorwegianBlueParrot.java
@@ -6,8 +6,6 @@ public class NorwegianBlueParrot extends Parrot {
 
     public NorwegianBlueParrot(double voltage, boolean isNailed) {
 
-        super(ParrotTypeEnum.NORWEGIAN_BLUE, 0, voltage, isNailed);
-
         this.isNailed = isNailed;
         this.voltage = voltage;
     }
