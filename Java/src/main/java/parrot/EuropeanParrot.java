5c84e0934c2c0331da45dec03251b93bc0955bf0
Andreas Denkmayr <andreas.denkmayr@gmail.com>
2021-12-23 11:42:00 +0100

    refactor: override getSpeed

--- Java/src/main/java/parrot/EuropeanParrot.java
+++ Java/src/main/java/parrot/EuropeanParrot.java
@@ -8,6 +8,6 @@ public class EuropeanParrot extends Parrot implements Speedable{
 
     @Override
     public double getSpeed() {
-        return super.getSpeed();
+        return BASE_SPEED;
     }
 }
