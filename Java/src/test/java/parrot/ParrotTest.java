16446c70fd4b227460661a652f6df57c7db86479
Günther Bernsteiner <gb@untis.at>
2021-12-23 12:47:15 +0100

    Calculator norwegian tests

--- Java/src/test/java/parrot/ParrotTest.java
+++ Java/src/test/java/parrot/ParrotTest.java
@@ -29,4 +29,21 @@ public class ParrotTest {
         assertEquals(12.0, parrot.getSpeed(), 0.0);
     }
 
+    @Test
+    public void getSpeedNorwegianBlueParrot_nailed() {
+        Calculator parrot = new Calculator(new NorwegianBlueParrot(1.5, true));
+        assertEquals(0.0, parrot.getSpeed(), 0.0);
+    }
+
+    @Test
+    public void getSpeedNorwegianBlueParrot_not_nailed() {
+        Calculator parrot = new Calculator(new NorwegianBlueParrot(1.5, false));
+        assertEquals(18.0, parrot.getSpeed(), 0.0);
+    }
+
+    @Test
+    public void getSpeedNorwegianBlueParrot_not_nailed_high_voltage() {
+        Calculator parrot = new Calculator(new NorwegianBlueParrot(4, false));
+        assertEquals(24.0, parrot.getSpeed(), 0.0);
+    }
 }
