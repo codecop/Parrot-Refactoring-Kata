39e0b4cc25ab9a39defa4972fdeb2742de163128
Andreas Denkmayr <andreas.denkmayr@gmail.com>
2021-12-23 11:54:34 +0100

    refactor: copied voltage and isNailed

--- Java/src/main/java/parrot/NorwegianBlueParrot.java
+++ Java/src/main/java/parrot/NorwegianBlueParrot.java
@@ -1,8 +1,13 @@
 package parrot;
 
-public class NorwegianBlueParrot extends Parrot implements Speedable{
+public class NorwegianBlueParrot extends Parrot implements Speedable {
+    private double voltage;
+    private boolean isNailed;
+
     public NorwegianBlueParrot(ParrotType parrotType, int numberOfCoconuts, double voltage, boolean isNailed) {
         super(parrotType, numberOfCoconuts, voltage, isNailed);
+        this.voltage = voltage;
+        this.isNailed = isNailed;
     }
 
     @Override
