f13df995b5299b8906e4d3ba55201291b48a6317
Martin Blum <martin.blum@untis.at>
2021-12-23 10:56:51 +0100

    remove parameter assignment

--- Java/src/main/java/parrot/Parrot.java
+++ Java/src/main/java/parrot/Parrot.java
@@ -11,7 +11,6 @@ public class Parrot {
     private ParrotSpeedCalulator parrotSpeedCalulator;
 
     public Parrot(ParrotTypeEnum type, int numberOfCoconuts, double voltage, boolean isNailed, ParrotSpeedCalulator parrotSpeedCalulator) {
-        this.type = type;
         this.numberOfCoconuts = numberOfCoconuts;
         this.voltage = voltage;
         this.isNailed = isNailed;
