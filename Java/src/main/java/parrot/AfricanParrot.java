6d8d372b1372d328fbe0be4b4162e4c18b54ee5c
Andreas Denkmayr <andreas.denkmayr@gmail.com>
2021-12-23 11:51:50 +0100

    refactor: removed unused constructor

--- Java/src/main/java/parrot/AfricanParrot.java
+++ Java/src/main/java/parrot/AfricanParrot.java
@@ -7,11 +7,6 @@ public class AfricanParrot extends Parrot implements Speedable{
 
     private final int numberOfCoconuts;
 
-    public AfricanParrot(ParrotType parrotType, int numberOfCoconuts, double voltage, boolean isNailed) {
-        super(parrotType, numberOfCoconuts, voltage, isNailed);
-        this.numberOfCoconuts = numberOfCoconuts;
-    }
-
     @Override
     public double getSpeed() {
         return Math.max(STANDING_IN_PLACE, BASE_SPEED - LOAD_FACTOR * numberOfCoconuts);
