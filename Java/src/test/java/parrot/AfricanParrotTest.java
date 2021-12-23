492f69936bce3970f24edb1b8cf37d2ff2a4125f
Stefan Wagner <stw@untis.at>
2021-12-23 11:31:39 +0100

    african test

--- Java/src/test/java/parrot/AfricanParrotTest.java
+++ Java/src/test/java/parrot/AfricanParrotTest.java
@@ -11,4 +11,16 @@ public class AfricanParrotTest {
         Parrot parrot = new AfricanParrot(1);
         assertEquals(3.0, parrot.getSpeed(), 0.0);
     }
+
+    @Test
+    public void getSpeedOfAfricanParrot_With_Two_Coconuts() {
+        Parrot parrot = new AfricanParrot(2);
+        assertEquals(0.0, parrot.getSpeed(), 0.0);
+    }
+
+    @Test
+    public void getSpeedOfAfricanParrot_With_No_Coconuts() {
+        Parrot parrot = new AfricanParrot(0);
+        assertEquals(12.0, parrot.getSpeed(), 0.0);
+    }
 }
