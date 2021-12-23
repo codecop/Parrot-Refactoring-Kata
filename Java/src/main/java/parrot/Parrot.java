9a7e6bf51765e7f9a10c6222a6d8b6b829b97fd9
Stefan Wagner <stw@untis.at>
2021-12-23 12:03:06 +0100

    parent class abstract

--- Java/src/main/java/parrot/Parrot.java
+++ Java/src/main/java/parrot/Parrot.java
@@ -3,7 +3,7 @@ package parrot;
 /**
  *
  */
-public class Parrot {
+public abstract class Parrot {
 
     private ParrotTypeEnum type;
     private int numberOfCoconuts;
@@ -20,18 +20,7 @@ public class Parrot {
 //        }
 //    }
 
-    public double getSpeed() {
-        switch (type) {
-            case EUROPEAN:
-                return getBaseSpeed();
-            case AFRICAN:
-                return Math.max(0, getBaseSpeed() - getLoadFactor() * numberOfCoconuts);
-            case NORWEGIAN_BLUE:
-                return (isNailed) ? 0 : getBaseSpeed(voltage);
-            default:
-                throw new RuntimeException("Should be unreachable");
-        }
-    }
+    abstract public double getSpeed();
 
      private double getBaseSpeed(double voltage) {
         return Math.min(24.0, voltage * getBaseSpeed());
