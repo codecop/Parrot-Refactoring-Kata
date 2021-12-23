23720515e758c7ad396a10bbbb790d1fc6ba7345
Andreas Denkmayr <andreas.denkmayr@gmail.com>
2021-12-23 11:39:43 +0100

    refactor: EuropeanParrot implements Speedable

--- Java/src/main/java/parrot/EuropeanParrot.java
+++ Java/src/main/java/parrot/EuropeanParrot.java
@@ -1,6 +1,6 @@
 package parrot;
 
-public class EuropeanParrot extends Parrot{
+public class EuropeanParrot extends Parrot implements Speedable{
 
     public EuropeanParrot(ParrotType parrotType, int numberOfCoconuts, double voltage, boolean isNailed) {
         super(parrotType, numberOfCoconuts, voltage, isNailed);
