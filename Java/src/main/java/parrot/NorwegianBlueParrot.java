9e6864c4931e5a0f919a58e94d95608353b78046
Andreas Denkmayr <andreas.denkmayr@gmail.com>
2021-12-23 11:38:41 +0100

    refactor: NorwegianBlueParrot extend Parrot

--- Java/src/main/java/parrot/NorwegianBlueParrot.java
+++ Java/src/main/java/parrot/NorwegianBlueParrot.java
@@ -1,4 +1,7 @@
 package parrot;
 
-public class NorwegianBlueParrot {
+public class NorwegianBlueParrot extends Parrot{
+    public NorwegianBlueParrot(ParrotType parrotType, int numberOfCoconuts, double voltage, boolean isNailed) {
+        super(parrotType, numberOfCoconuts, voltage, isNailed);
+    }
 }
