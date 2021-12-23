a6a9157c50f8a47e52f39f638019fe2919da874b
Martin Blum <martin.blum@untis.at>
2021-12-23 10:27:13 +0100

    documentation

--- Java/src/main/java/parrot/speedcalculators/AfricanParrotSpeedCalculator.java
+++ Java/src/main/java/parrot/speedcalculators/AfricanParrotSpeedCalculator.java
@@ -1,5 +1,6 @@
 package parrot.speedcalculators;
 
+// Math.max(0, getBaseSpeed() - getLoadFactor() * numberOfCoconuts);
 public class AfricanParrotSpeedCalculator implements ParrotSpeedCalulator {
     @Override
     public double getSpeed() {
