f0034036295af18765344617e67c8d64e8c3ba00
Andreas Denkmayr <andreas.denkmayr@gmail.com>
2021-12-23 11:52:44 +0100

    refactor: reformatting

--- Java/src/main/java/parrot/EuropeanParrot.java
+++ Java/src/main/java/parrot/EuropeanParrot.java
@@ -3,7 +3,7 @@ package parrot;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
-public class EuropeanParrot extends Parrot implements Speedable{
+public class EuropeanParrot extends Parrot implements Speedable {
 
     @Override
     public double getSpeed() {
