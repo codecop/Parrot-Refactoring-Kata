6393fbc36ea9fb2258b4fc4223726abadf96ab32
Stefan Wagner <stw@untis.at>
2021-12-23 11:39:38 +0100

    norwegian tests

--- Java/src/test/java/parrot/NorwegianBlueParrotTest.java
+++ Java/src/test/java/parrot/NorwegianBlueParrotTest.java
@@ -1,4 +1,26 @@
 package parrot;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class NorwegianBlueParrotTest {
+
+    @Test
+    public void getSpeedNorwegianBlueParrot_nailed() {
+        Parrot parrot = new NorwegianBlueParrot(1.5, true);
+        assertEquals(0.0, parrot.getSpeed(), 0.0);
+    }
+
+    @Test
+    public void getSpeedNorwegianBlueParrot_not_nailed() {
+        Parrot parrot = new NorwegianBlueParrot(1.5, false);
+        assertEquals(18.0, parrot.getSpeed(), 0.0);
+    }
+
+    @Test
+    public void getSpeedNorwegianBlueParrot_not_nailed_high_voltage() {
+        Parrot parrot = new NorwegianBlueParrot(4, false);
+        assertEquals(24.0, parrot.getSpeed(), 0.0);
+    }
 }
