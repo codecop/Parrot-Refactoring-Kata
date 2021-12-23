51037f14dc0bf00677b83d2bc370911e69e58f5b
Stefan Wagner <stw@untis.at>
2021-12-23 11:03:15 +0100

    african constructor

--- Java/src/main/java/parrot/AfricanParrot.java
+++ Java/src/main/java/parrot/AfricanParrot.java
@@ -5,9 +5,10 @@ package parrot;
  * @since 23.12.2021
  */
 public class AfricanParrot extends Parrot {
+
     private int numberOfCoconuts;
 
-    public double getSpeed() {
-        return 0.0;
+    public AfricanParrot(int numberOfCoconuts) {
+        this.numberOfCoconuts = numberOfCoconuts;
     }
 }
---
+++ Java/src/test/java/parrot/AfricanParrotTest.java
@@ -0,0 +1,10 @@
+package parrot;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class AfricanParrotTest {
+
+
+}
