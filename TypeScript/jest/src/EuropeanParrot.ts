a2706a476c54f2d269673227a6f1116fcf5b1739
Alexander Springer <a.springer@sportradar.com>
2022-03-01 15:15:19 +0100

    removed obsolete extension of Parrot in European Parrot

--- TypeScript/jest/src/EuropeanParrot.ts
+++ TypeScript/jest/src/EuropeanParrot.ts
@@ -1,12 +1,7 @@
-import { Parrot} from './parrot';
 import { IParrot } from './IParrot';
 import { PARROT_BASE_SPEED } from './ParrotBaseSpeed';
 
-export class EuropeanParrot extends Parrot implements IParrot {
-    constructor() {
-        super();
-    }
-
+export class EuropeanParrot implements IParrot {
     public getSpeed(): number {
         return PARROT_BASE_SPEED;
     }
