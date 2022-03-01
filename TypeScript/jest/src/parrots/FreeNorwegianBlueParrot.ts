67b3b0ca406763ef8fbdba7a275954ba7ce85a0a
Alexander Springer <a.springer@sportradar.com>
2022-03-01 15:28:05 +0100

    extracted minimumSpeed constant

--- TypeScript/jest/src/parrots/FreeNorwegianBlueParrot.ts
+++ TypeScript/jest/src/parrots/FreeNorwegianBlueParrot.ts
@@ -2,10 +2,12 @@ import { IParrot } from './IParrot';
 import { PARROT_BASE_SPEED } from './ParrotBaseSpeed';
 
 export class FreeNorwegianBlueParrot implements IParrot {
+    private static readonly minimumSpeed = 24;
+
     constructor(private voltage: number) {
     }
 
     public getSpeed(): number {
-        return Math.min(24, this.voltage * PARROT_BASE_SPEED);
+        return Math.min(FreeNorwegianBlueParrot.minimumSpeed, this.voltage * PARROT_BASE_SPEED);
     }
 }
