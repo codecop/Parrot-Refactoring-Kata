9ac733d54fb21e8801ea1996d8161991c1c9e12b
b.singh <b.singh@sportradar.com>
2022-03-01 15:12:46 +0100

    Set voltage for norwegian blue parrot.

--- Java/src/main/java/parrot/NorwegianBlueParrot.java
+++ Java/src/main/java/parrot/NorwegianBlueParrot.java
@@ -6,6 +6,7 @@ public class NorwegianBlueParrot extends Parrot {
 
     public NorwegianBlueParrot(ParrotTypeEnum type, int numberOfCoconuts, double voltage, boolean isNailed) {
         super(type, numberOfCoconuts, voltage, isNailed);
+        this.voltage = voltage;
     }
 
     @Override
