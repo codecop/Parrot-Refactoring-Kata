9fc0f4c328e8a1989054f41acaa7d6c364aa2cd7
o.husar <o.husar@sportradar.com>
2022-03-01 14:52:25 +0100

    Implement getSpeed in NorwegianBlueParrot

--- TypeScript/jest/src/norwegianBlueParrot.ts
+++ TypeScript/jest/src/norwegianBlueParrot.ts
@@ -4,6 +4,10 @@ class NorwegianBlueParrot extends Parrot {
     constructor(numberOfCoconuts: number, voltage: number, isNailed: boolean) {
         super(ParrotTypes.NORWEGIAN_BLUE, numberOfCoconuts, voltage, isNailed);
     }
+
+    getSpeed(): number {
+        return super.getSpeed();
+    }
 }
 
 export default NorwegianBlueParrot;
