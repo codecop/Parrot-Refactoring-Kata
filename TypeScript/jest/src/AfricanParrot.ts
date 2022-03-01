aff0857e9843c53e3b0725cd7a3d4951cdb4e44d
n.pecanin <n.pecanin@sportradar.com>
2022-03-01 15:10:28 +0100

    Adds one field.

--- TypeScript/jest/src/AfricanParrot.ts
+++ TypeScript/jest/src/AfricanParrot.ts
@@ -5,6 +5,8 @@ export default class AfricanParrot extends AbstractParrot implements ISleepableP
     private loadFactor: number = 9;
     minSpeed: number = 0;
 
+    private countOfCoconuts: number = 0;
+
     constructor(numberOfCoconuts: number, voltage: number, isNailed: boolean) {
         super(numberOfCoconuts, voltage, isNailed);
     }
