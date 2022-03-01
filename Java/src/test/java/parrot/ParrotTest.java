f66ec4a48255094c7cb86d24dd7a744274cdebdc
b.singh <b.singh@sportradar.com>
2022-03-01 15:19:55 +0100

    Remove getSpeedOfAfricanParrot_With_One_Coconut.

--- Java/src/test/java/parrot/ParrotTest.java
+++ Java/src/test/java/parrot/ParrotTest.java
@@ -11,10 +11,4 @@ public class ParrotTest {
         Parrot parrot = new Parrot(ParrotTypeEnum.EUROPEAN, 0, 0, false);
         assertEquals(12.0, parrot.getSpeed(), 0.0);
     }
-
-    @Test
-    public void getSpeedOfAfricanParrot_With_One_Coconut() {
-        Parrot parrot = new Parrot(ParrotTypeEnum.AFRICAN, 1, 0, false);
-        assertEquals(3.0, parrot.getSpeed(), 0.0);
-    }
 }
