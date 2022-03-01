47c9dc8a9b082fda8b58757d303ce5b51cae3c23
Paul Opuchlich <p.opuchlich@sportradar.com>
2022-03-01 14:55:06 +0100

    refactor test names

--- Python/test_parrot.py
+++ Python/test_parrot.py
@@ -1,30 +1,30 @@
 
 from parrot import NorwegianParrot, AfricanParrot, EuropeanParrot
 
-def test_speedNorwegianBlueParrot_not_nailed_high_voltage2():
+def test_speedNorwegianBlueParrot_not_nailed_high_voltage():
     parrot = NorwegianParrot(4, False)
     assert parrot.speed() == 24.0
 
-def test_speedNorwegianBlueParrot_not_nailed2():
+def test_speedNorwegianBlueParrot_not_nailed():
     parrot = NorwegianParrot(1.5, False)
     assert parrot.speed() == 18.0
 
-def test_speedNorwegianBlueParrot_nailed2():
+def test_speedNorwegianBlueParrot_nailed():
     parrot = NorwegianParrot(1.5, True)
     assert parrot.speed() == 0.0
 
-def test_speedOfAfricanParrot_With_No_Coconuts2():
+def test_speedOfAfricanParrot_With_No_Coconuts():
     parrot = AfricanParrot(0)
     assert parrot.speed() == 12.0
 
-def test_speedOfAfricanParrot_With_Two_Coconuts2():
+def test_speedOfAfricanParrot_With_Two_Coconuts():
     parrot = AfricanParrot(2)
     assert parrot.speed() == 0.0
 
-def test_speedOfAfricanParrot_With_One_Coconut2():
+def test_speedOfAfricanParrot_With_One_Coconut():
     parrot = AfricanParrot(1)
     assert parrot.speed() == 3.0
 
-def test_speedOfEuropeanParrot2():
+def test_speedOfEuropeanParrot():
     parrot = EuropeanParrot()
     assert parrot.speed() == 12.0
