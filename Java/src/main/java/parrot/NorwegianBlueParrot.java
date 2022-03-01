ab8fe3507f67fb689633105adfad2a5c0504ead0
b.singh <b.singh@sportradar.com>
2022-03-01 15:05:54 +0100

    Override get speed for norwegian blue parrot.

--- Java/src/main/java/parrot/NorwegianBlueParrot.java
+++ Java/src/main/java/parrot/NorwegianBlueParrot.java
@@ -1,7 +1,12 @@
 package parrot;
 
-public class NorwegianBlueParrot extends Parrot{
+public class NorwegianBlueParrot extends Parrot {
     public NorwegianBlueParrot(ParrotTypeEnum type, int numberOfCoconuts, double voltage, boolean isNailed) {
         super(type, numberOfCoconuts, voltage, isNailed);
+    }
+
+    @Override
+    public double getSpeed() {
+        return super.getSpeed();
     }
 }
