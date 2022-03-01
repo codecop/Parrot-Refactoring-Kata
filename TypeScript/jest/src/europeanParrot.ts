e0678aa737dedcc8aaba621527389c4be8326763
o.husar <o.husar@sportradar.com>
2022-03-01 15:33:09 +0100

    Remove redundant constructor from EuropeanParrot

--- TypeScript/jest/src/europeanParrot.ts
+++ TypeScript/jest/src/europeanParrot.ts
@@ -1,10 +1,6 @@
 import {Parrot} from './parrot';
 
 class EuropeanParrot extends Parrot {
-    constructor() {
-        super();
-    }
-
     getSpeed(): number {
         return super.getBaseSpeed();
     }
