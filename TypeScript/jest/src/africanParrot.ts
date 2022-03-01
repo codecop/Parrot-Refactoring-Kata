ae82459cc46d2513899ae5870f78e5a67eb5e74b
o.husar <o.husar@sportradar.com>
2022-03-01 14:53:56 +0100

    Implement getSpeed in AfricanParrot

--- TypeScript/jest/src/africanParrot.ts
+++ TypeScript/jest/src/africanParrot.ts
@@ -4,6 +4,10 @@ class AfricanParrot extends Parrot {
     constructor(numberOfCoconuts: number, voltage: number, isNailed: boolean) {
         super(ParrotTypes.AFRICAN, numberOfCoconuts, voltage, isNailed);
     }
+
+    getSpeed(): number {
+        return super.getSpeed();
+    }
 }
 
 export default AfricanParrot;
