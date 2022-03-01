324e49064cfb16a19efc93331855e2f64e092e4e
s.lukas <s.lukas@sportradar.com>
2022-03-01 14:36:28 +0100

    Norwegian blue parrot has voltage and isNailed properties.

--- TypeScript/jest/src/parrots/norwegian-blue-parrot.ts
+++ TypeScript/jest/src/parrots/norwegian-blue-parrot.ts
@@ -1,6 +1,16 @@
 import AbstractParrot from '../abstract-parrot';
 
 export default class NorwegianBlueParrot extends AbstractParrot{
+
+    private voltage: number;
+    private isNailed: boolean;
+
+    constructor(voltage: number, isNailed: boolean) {
+        super();
+        this.voltage = voltage;
+        this.isNailed = isNailed;
+    }
+
     getSpeed(): number {
         return 0;
     }
