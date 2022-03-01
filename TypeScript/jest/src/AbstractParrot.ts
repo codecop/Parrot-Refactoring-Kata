864821954ecac12bdc7b0a940617467839ffac43
n.pecanin <n.pecanin@sportradar.com>
2022-03-01 14:20:38 +0100

    Introduce abstract class with constructor.

---
+++ TypeScript/jest/src/AbstractParrot.ts
@@ -0,0 +1,11 @@
+abstract class AbstractParrot {
+    private readonly numberOfCoconuts: number;
+    private readonly voltage: number;
+    private readonly isNailed: boolean;
+
+    protected constructor(numberOfCoconuts: number, voltage: number, isNailed: boolean) {
+        this.numberOfCoconuts = numberOfCoconuts;
+        this.voltage = voltage;
+        this.isNailed = isNailed;
+    }
+}
