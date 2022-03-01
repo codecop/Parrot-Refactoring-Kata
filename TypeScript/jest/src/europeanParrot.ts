80163e6d1c27dd29b0fd83058285fb195808be3b
o.husar <o.husar@sportradar.com>
2022-03-01 14:49:02 +0100

    Implement speed getter in EuropeanParrot

--- TypeScript/jest/src/europeanParrot.ts
+++ TypeScript/jest/src/europeanParrot.ts
@@ -4,6 +4,10 @@ class EuropeanParrot extends Parrot {
     constructor(numberOfCoconuts: number, voltage: number, isNailed: boolean) {
         super(ParrotTypes.EUROPEAN, numberOfCoconuts, voltage, isNailed);
     }
+
+    getSpeed(): number {
+        return super.getBaseSpeed();
+    }
 }
 
 export default EuropeanParrot;
