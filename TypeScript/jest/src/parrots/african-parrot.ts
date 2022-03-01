5d7dcf258788b81bed873eed1841e9a00d3984ec
s.lukas <s.lukas@sportradar.com>
2022-03-01 15:12:54 +0100

    Introduces min speed constant on african parrot.

--- TypeScript/jest/src/parrots/african-parrot.ts
+++ TypeScript/jest/src/parrots/african-parrot.ts
@@ -2,6 +2,8 @@ import AbstractParrot from '../abstract-parrot';
 
 export class AfricanParrot extends AbstractParrot {
 
+    private readonly MIN_SPEED = 0;
+
     private readonly LOAD_FACTOR = 9;
 
     private readonly numberOfCoconuts: number;
@@ -12,7 +14,7 @@ export class AfricanParrot extends AbstractParrot {
     }
 
     getSpeed(): number {
-        return  Math.max(0, this.getBaseSpeed() - this.getLoadFactor() * this.numberOfCoconuts);
+        return  Math.max(this.MIN_SPEED, this.getBaseSpeed() - this.getLoadFactor() * this.numberOfCoconuts);
     }
 
     private getLoadFactor(): number {
