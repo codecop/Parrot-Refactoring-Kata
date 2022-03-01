9a920fa7e59e9e181edc1376384735462c20d91a
b.singh <b.singh@sportradar.com>
2022-03-01 14:30:22 +0100

    Add get speed test for european parrot.

--- Java/src/test/java/parrot/EuropeanParrotTest.java
+++ Java/src/test/java/parrot/EuropeanParrotTest.java
@@ -1,7 +1,17 @@
 package parrot;
 
-import junit.framework.TestCase;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
 
-public class EuropeanParrotTest extends TestCase {
+public class EuropeanParrotTest {
+
+    @Test
+    public void shouldReturnSpeed(){
+        EuropeanParrot europeanParrot = new EuropeanParrot(ParrotTypeEnum.EUROPEAN, 0, 0, false);
+
+        double europeanParrotSpeed = europeanParrot.getSpeed();
+
+        assertEquals(12.0, europeanParrotSpeed, 0.0);
+    }
 
 }
