66338c6b813ec96273b4f1c71e0be094c25ee9e3
s.lukas <s.lukas@sportradar.com>
2022-03-01 14:30:48 +0100

    African parrot has coconuts.

--- TypeScript/jest/src/parrots/african-parrot.ts
+++ TypeScript/jest/src/parrots/african-parrot.ts
@@ -1,6 +1,14 @@
 import AbstractParrot from '../abstract-parrot';
 
 export class AfricanParrot extends AbstractParrot {
+
+    private numberOfCoconuts: number;
+
+    constructor(numberOfCoconuts: number) {
+        super();
+        this.numberOfCoconuts = numberOfCoconuts;
+    }
+
     getSpeed(): number {
         return 0;
     }
