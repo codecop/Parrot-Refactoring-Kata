6d1770691467c6507b765e7ba32f7cc2576bf198
Paul Opuchlich <p.opuchlich@sportradar.com>
2022-03-01 15:31:40 +0100

    remove un-parameterized test for african parrot

--- Python/test_parrot.py
+++ Python/test_parrot.py
@@ -27,18 +27,6 @@ def test_speedNorwegianBlueParrot_nailed():
     norwegian_parrot = NorwegianParrot(1.5, True)
     assert norwegian_parrot.speed() == 0.0
 
-def test_speedOfAfricanParrot_With_No_Coconuts():
-    parrot = AfricanParrot(0)
-    assert parrot.speed() == 12.0
-
-def test_speedOfAfricanParrot_With_Two_Coconuts():
-    african_parrot = AfricanParrot(2)
-    assert african_parrot.speed() == 0.0
-
-def test_speedOfAfricanParrot_With_One_Coconut():
-    african_parrot = AfricanParrot(1)
-    assert african_parrot.speed() == 3.0
-
 def test_speedOfEuropeanParrot():
     european_parrot = EuropeanParrot()
     assert european_parrot.speed() == 12.0
