a1cae6b3a1836747a9069a8f9290a582c3e33016
b.singh <b.singh@sportradar.com>
2022-03-01 15:13:03 +0100

    Set is nailed for norwegian blue parrot.

--- Java/src/main/java/parrot/NorwegianBlueParrot.java
+++ Java/src/main/java/parrot/NorwegianBlueParrot.java
@@ -7,6 +7,7 @@ public class NorwegianBlueParrot extends Parrot {
     public NorwegianBlueParrot(ParrotTypeEnum type, int numberOfCoconuts, double voltage, boolean isNailed) {
         super(type, numberOfCoconuts, voltage, isNailed);
         this.voltage = voltage;
+        this.isNailed = isNailed;
     }
 
     @Override
