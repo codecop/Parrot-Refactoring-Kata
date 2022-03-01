4ce98a663885b48a25ebaceeeca5ad4e771b648d
n.pecanin <n.pecanin@sportradar.com>
2022-03-01 14:58:04 +0100

    Increase new const for magic number.

--- TypeScript/jest/src/AfricanParrot.ts
+++ TypeScript/jest/src/AfricanParrot.ts
@@ -2,12 +2,13 @@ import {AbstractParrot} from "./AbstractParrot";
 
 export default class AfricanParrot extends AbstractParrot {
     private loadFactor: number = 9;
+    private minSpeed: number = 0;
 
     constructor(numberOfCoconuts: number, voltage: number, isNailed: boolean) {
         super(numberOfCoconuts, voltage, isNailed);
     }
 
     getSpeed(): number {
-        return Math.max(0, this.baseSpeed - this.loadFactor * this.numberOfCoconuts);
+        return Math.max(this.minSpeed, this.baseSpeed - this.loadFactor * this.numberOfCoconuts);
     }
 }
