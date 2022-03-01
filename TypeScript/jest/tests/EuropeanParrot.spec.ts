1860e54c72e1d7d41ddcf566f82ff472f5d801bf
n.pecanin <n.pecanin@sportradar.com>
2022-03-01 14:51:37 +0100

    adds test

--- TypeScript/jest/tests/EuropeanParrot.spec.ts
+++ TypeScript/jest/tests/EuropeanParrot.spec.ts
@@ -2,6 +2,11 @@ import EuropeanParrot from "../src/EuropeanParrot";
 
 describe('EuropeanParrot', () => {
 
+    it('should be instance of European Parrot.', () => {
+        const europeanParrot = new EuropeanParrot(0, 0, false);
+        expect(europeanParrot).toBeInstanceOf(EuropeanParrot);
+    });
+
     it('gets speed of European Parrot', () => {
         const europeanParrot = new EuropeanParrot(0, 0, false);
         expect(europeanParrot.getSpeed()).toBe(12);
