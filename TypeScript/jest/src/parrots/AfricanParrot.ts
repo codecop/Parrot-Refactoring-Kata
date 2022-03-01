ce0064d4969da7f16fb6fd1044a5c8e84558fc22
Alexander Springer <a.springer@sportradar.com>
2022-03-01 15:29:35 +0100

    extracted minimumSpeed constant in African Parrot

--- TypeScript/jest/src/parrots/AfricanParrot.ts
+++ TypeScript/jest/src/parrots/AfricanParrot.ts
@@ -4,11 +4,13 @@ import { PARROT_BASE_SPEED } from './ParrotBaseSpeed';
 const LOAD_FACTOR = 9;
 
 export class AfricanParrot implements IParrot {
+    private static readonly minimumSpeed = 0;
+
     constructor(private numberOfCoconuts: number) {
     }
 
     public getSpeed(): number {
         const weightSpeedPenalty = LOAD_FACTOR * this.numberOfCoconuts;
-        return Math.max(0, PARROT_BASE_SPEED - weightSpeedPenalty);
+        return Math.max(AfricanParrot.minimumSpeed, PARROT_BASE_SPEED - weightSpeedPenalty);
     }
 }
