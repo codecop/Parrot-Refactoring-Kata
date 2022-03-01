6d30f61a723fc6c8d9cb5dfc6d9df14b25621958
Paul Opuchlich <p.opuchlich@sportradar.com>
2022-03-01 14:58:25 +0100

    add base speed constant

--- Python/parrot.py
+++ Python/parrot.py
@@ -1,11 +1,13 @@
 from abc import abstractmethod
 
+BASE_SPEED = 12.0
+
 class IParrot:
     @abstractmethod
     def speed(self): raise NotImplementedError
     @classmethod 
     def _base_speed(self):
-        return 12.0
+        return BASE_SPEED
 
 class EuropeanParrot(IParrot):
 
