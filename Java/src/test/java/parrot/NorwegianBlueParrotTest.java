97dfe78d946c508156e47c4fcbbdc9951e8953f9
b.singh <b.singh@sportradar.com>
2022-03-01 14:39:49 +0100

    Add get speed for norwegian parrot nailed.

--- Java/src/test/java/parrot/NorwegianBlueParrotTest.java
+++ Java/src/test/java/parrot/NorwegianBlueParrotTest.java
@@ -1,7 +1,20 @@
 package parrot;
 
-import junit.framework.TestCase;
 
-public class NorwegianBlueParrotTest extends TestCase {
+import org.junit.Assert;
+import org.junit.Test;
+
+
+
+public class NorwegianBlueParrotTest {
+
+    @Test
+    public void shouldReturnSpeedNailed(){
+        NorwegianBlueParrot norwegianBlueParrot = new NorwegianBlueParrot(ParrotTypeEnum.NORWEGIAN_BLUE, 0, 0, true);
+
+        double norwegianBlueParrotSpeed = norwegianBlueParrot.getSpeed();
+
+        Assert.assertEquals(0.0, norwegianBlueParrotSpeed, 0.0);
+    }
 
 }
