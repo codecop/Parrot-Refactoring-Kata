84d4e2180dc61f43b547c85e3c97e13038abd285
o.husar <o.husar@sportradar.com>
2022-03-01 14:25:10 +0100

    AfricanParrot is of the AFRICAN type

--- TypeScript/jest/src/africanParrot.ts
+++ TypeScript/jest/src/africanParrot.ts
@@ -1,3 +1,9 @@
-import {Parrot} from './parrot';
+import {Parrot, ParrotTypes} from './parrot';
 
-class AfricanParrot extends Parrot {}
+class AfricanParrot extends Parrot {
+    constructor(numberOfCoconuts: number, voltage: number, isNailed: boolean) {
+        super(ParrotTypes.AFRICAN, numberOfCoconuts, voltage, isNailed);
+    }
+}
+
+export default AfricanParrot;
