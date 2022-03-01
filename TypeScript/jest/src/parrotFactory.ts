3963eaf151748555dc75dd5e7d633c0fa07be996
Alexander Springer <a.springer@sportradar.com>
2022-03-01 14:21:11 +0100

    add return types to ParrotFactory

--- TypeScript/jest/src/parrotFactory.ts
+++ TypeScript/jest/src/parrotFactory.ts
@@ -1,15 +1,15 @@
 import { Parrot, ParrotTypes } from './parrot';
 
 export class ParrotFactory {
-    public createEuropeanParrot() {
+    public createEuropeanParrot(): Parrot {
         return new Parrot(ParrotTypes.EUROPEAN, 0, 0, false);
     }
 
-    public createAfricanParrot(numberOfCoconuts: number) {
+    public createAfricanParrot(numberOfCoconuts: number): Parrot {
         return new Parrot(ParrotTypes.AFRICAN, numberOfCoconuts, 0, false);
     }
 
-    public createNorwegianParrot(voltage: number, isNailed: boolean) {
+    public createNorwegianParrot(voltage: number, isNailed: boolean): Parrot {
         return new Parrot(ParrotTypes.NORWEGIAN_BLUE, 0, voltage, isNailed);
     }
 }
