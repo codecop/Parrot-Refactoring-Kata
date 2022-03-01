3b2fbcceb5d38a9e7f3d48349e10621b5f928eac
Paul Opuchlich <p.opuchlich@sportradar.com>
2022-03-01 15:03:46 +0100

    add constant max voltage speed

--- Python/parrot.py
+++ Python/parrot.py
@@ -3,6 +3,7 @@ from abc import abstractmethod
 BASE_SPEED = 12.0
 LOAD_FACTOR = 9.0
 NAILED_SPEED = 0
+MAXIMUM_VOLTAGE_SPEED = 24.0
 
 class IParrot:
     @abstractmethod
@@ -43,4 +44,4 @@ class NorwegianParrot(IParrot):
             return self._compute_base_speed_for_voltage(self._voltage)
 
     def _compute_base_speed_for_voltage(self, voltage):
-        return min([24.0, voltage * self._base_speed()])
+        return min([MAXIMUM_VOLTAGE_SPEED, voltage * self._base_speed()])
