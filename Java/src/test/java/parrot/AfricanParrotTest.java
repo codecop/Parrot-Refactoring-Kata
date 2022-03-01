e1ff22866736bf0c8afb8e89ca9dd2ebfa444a17
b.singh <b.singh@sportradar.com>
2022-03-01 14:34:15 +0100

    Add get speed with one coconut for african parrot.

--- Java/src/test/java/parrot/AfricanParrotTest.java
+++ Java/src/test/java/parrot/AfricanParrotTest.java
@@ -1,7 +1,18 @@
 package parrot;
 
-import junit.framework.TestCase;
+import org.junit.Assert;
+import org.junit.Test;
 
-public class AfricanParrotTest extends TestCase {
+
+public class AfricanParrotTest {
+
+    @Test
+    public void shouldReturnSpeedWithOneCoconut(){
+        AfricanParrot africanParrot = new AfricanParrot(ParrotTypeEnum.AFRICAN, 1, 0, false);
+
+        double africanParrotSpeed = africanParrot.getSpeed();
+
+        Assert.assertEquals(3.0, africanParrotSpeed, 0.0);
+    }
 
 }
