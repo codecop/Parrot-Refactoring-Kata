a0d8c5d2f990ad04f95a77561bb598d07d426dbe
Paul Opuchlich <p.opuchlich@sportradar.com>
2022-03-01 15:36:47 +0100

    remove unused tests

--- Python/test_parrot.py
+++ Python/test_parrot.py
@@ -25,19 +25,6 @@ class NorwegianParrotTestSequence(unittest.TestCase):
         norwegian_parrot = NorwegianParrot(voltage, is_nailed)
         assert norwegian_parrot.speed() == expected_speed
 
-
-def test_speedNorwegianBlueParrot_not_nailed_high_voltage():
-    norwegian_parrot = NorwegianParrot(4, False)
-    assert norwegian_parrot.speed() == 24.0
-
-def test_speedNorwegianBlueParrot_not_nailed():
-    norwegian_parrot = NorwegianParrot(1.5, False)
-    assert norwegian_parrot.speed() == 18.0
-
-def test_speedNorwegianBlueParrot_nailed():
-    norwegian_parrot = NorwegianParrot(1.5, True)
-    assert norwegian_parrot.speed() == 0.0
-
 def test_speedOfEuropeanParrot():
     european_parrot = EuropeanParrot()
     assert european_parrot.speed() == 12.0
