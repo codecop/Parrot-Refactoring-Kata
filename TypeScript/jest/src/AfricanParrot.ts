4d902ce25e4e2287d98e1d898cd578f8bf4f733c
n.pecanin <n.pecanin@sportradar.com>
2022-03-01 15:19:26 +0100

    Cleans constructors, uses countOfCoconuts.

--- TypeScript/jest/src/AfricanParrot.ts
+++ TypeScript/jest/src/AfricanParrot.ts
@@ -3,25 +3,17 @@ import ISleepableParrot from "./ISleepableParrot";
 
 export default class AfricanParrot extends AbstractParrot implements ISleepableParrot {
     private loadFactor: number = 9;
+
     minSpeed: number = 0;
 
-    private countOfCoconuts: number;
-
-    private voltageNumber: number;
-
-    private isNailedValue: boolean;
+    countOfCoconuts: number;
 
     constructor(numberOfCoconuts: number, voltage: number, isNailed: boolean) {
         super(numberOfCoconuts, voltage, isNailed);
-
         this.countOfCoconuts = numberOfCoconuts;
-
-        this.voltageNumber = voltage;
-
-        this.isNailedValue = isNailed;
     }
 
     getSpeed(): number {
-        return Math.max(this.minSpeed, this.baseSpeed - this.loadFactor * this.numberOfCoconuts);
+        return Math.max(this.minSpeed, this.baseSpeed - this.loadFactor * this.countOfCoconuts);
     }
 }
