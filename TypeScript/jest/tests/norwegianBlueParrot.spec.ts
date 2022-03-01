da07bfed140a94300791eeb3675a5e28be3bf144
o.husar <o.husar@sportradar.com>
2022-03-01 14:43:19 +0100

    Rename tests for NorwegianBlueParrot

--- TypeScript/jest/tests/norwegianBlueParrot.spec.ts
+++ TypeScript/jest/tests/norwegianBlueParrot.spec.ts
@@ -2,19 +2,18 @@ import {Parrot, ParrotTypes} from '../src/parrot';
 import NorwegianBlueParrot from '../src/norwegianBlueParrot';
 
 describe('NorwegianBlueParrot', () => {
-    it('gets speed of Norwegian Blue Parrot nailed', () => {
+    it('should have correct speed if it\'s nailed', () => {
         const parrot = new NorwegianBlueParrot( 0, 1.5, true);
         expect(parrot.getSpeed()).toBe(0);
     });
 
-    it('gets speed of Norwegian Blue Parrot not nailed', () => {
+    it('should have correct speed if it is not nailed', () => {
         const parrot = new NorwegianBlueParrot( 0, 1.5, false);
         expect(parrot.getSpeed()).toBe(18);
     });
 
-    it('gets speed of Norwegian Blue Parrot not nailed high voltage', () => {
+    it('should have correct speed if it is not nailed and has high voltage', () => {
         const parrot = new NorwegianBlueParrot( 0, 4, false);
         expect(parrot.getSpeed()).toBe(24);
     });
-
 })
