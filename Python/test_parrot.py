84efef699f52b078bd2458ad756aa3953f646cd1
Paul Opuchlich <p.opuchlich@sportradar.com>
2022-03-01 15:30:19 +0100

    add african test sequence with zero coconut

--- Python/test_parrot.py
+++ Python/test_parrot.py
@@ -1,7 +1,20 @@
 
+import unittest
 from european_parrot import EuropeanParrot
 from african_parrot import AfricanParrot
 from norwegian_parrot import NorwegianParrot
+from parameterized import parameterized
+
+class TestSequence(unittest.TestCase):
+    @parameterized.expand([
+        [0, 12.0],
+        [1, 3.0],
+        [2, 0.0]
+    ])
+    def test_sequence(self, coconut_number, expected_speed):
+        african_parrot = AfricanParrot(coconut_number)
+        assert african_parrot.speed() == expected_speed
+
 def test_speedNorwegianBlueParrot_not_nailed_high_voltage():
     norwegian_parrot = NorwegianParrot(4, False)
     assert norwegian_parrot.speed() == 24.0
