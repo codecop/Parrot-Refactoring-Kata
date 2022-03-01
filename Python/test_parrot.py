3e29e0af40ce88a9f65b6882530a68eee3f94e3e
Paul Opuchlich <p.opuchlich@sportradar.com>
2022-03-01 14:28:42 +0100

    norwegean test for nailed

--- Python/test_parrot.py
+++ Python/test_parrot.py
@@ -42,3 +42,7 @@ def test_speedNorwegianBlueParrot_not_nailed_high_voltage2():
 def test_speedNorwegianBlueParrot_not_nailed2():
     parrot = Norwegian_Parrot(1.5, False)
     assert parrot.speed() == 18.0
+
+def test_speedNorwegianBlueParrot_nailed2():
+    parrot = Norwegian_Parrot(1.5, True)
+    assert parrot.speed() == 0.0
