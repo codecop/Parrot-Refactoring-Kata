f47f6fd38dc28cd53230d1ab315b7d4fd45833b3
s.lukas <s.lukas@sportradar.com>
2022-03-01 14:50:21 +0100

    Move african parrot tests to standalone spec file.

--- TypeScript/jest/src/parrots/african-parrot.spec.ts
+++ TypeScript/jest/src/parrots/african-parrot.spec.ts
@@ -1,3 +1,18 @@
-describe('African Parrot', () => {
+import {AfricanParrot} from './african-parrot';
 
+describe('African Parrot', () => {
+    it('gets speed of African Parrot with one coconut', () => {
+        const parrot = new AfricanParrot(1);
+        expect(parrot.getSpeed()).toBe(3);
+    });
+
+    it('gets speed of African Parrot with two coconuts', () => {
+        const parrot = new AfricanParrot(2);
+        expect(parrot.getSpeed()).toBe(0);
+    });
+
+    it('gets speed of African Parrot with no coconuts', () => {
+        const parrot = new AfricanParrot(0);
+        expect(parrot.getSpeed()).toBe(12);
+    });
 });
--- TypeScript/jest/tests/parrot.spec.ts
+++ TypeScript/jest/tests/parrot.spec.ts
@@ -9,21 +9,6 @@ describe('Parrot', () => {
         expect(parrot.getSpeed()).toBe(12);
     });
 
-    it('gets speed of African Parrot with one coconut', () => {
-        const parrot = new AfricanParrot(1);
-        expect(parrot.getSpeed()).toBe(3);
-    });
-
-    it('gets speed of African Parrot with two coconuts', () => {
-        const parrot = new AfricanParrot(2);
-        expect(parrot.getSpeed()).toBe(0);
-    });
-
-    it('gets speed of African Parrot with no coconuts', () => {
-        const parrot = new AfricanParrot(0);
-        expect(parrot.getSpeed()).toBe(12);
-    });
-
     it('gets speed of Norwegian Blue Parrot nailed', () => {
         const parrot = new NorwegianBlueParrot(  1.5, true);
         expect(parrot.getSpeed()).toBe(0);
